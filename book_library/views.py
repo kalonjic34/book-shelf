113d7d5326e14b848195be79c3af06378c1c2ff3
Christian <kalonjic34@gmail.com>
2026-09-18 15:10:24 +0200

    add genres view to retrieve and display book genres

--- book_library/views.py
+++ book_library/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-from .models import Book
+from .models import Book,Genre
 
 def index(request):
     books = Book.objects.all()
     context = {"books":books}
     return render(request,'book_library/index.html',context)
+
+def genres(request):
+    genres = Genre.objects.all()
+    context = {"genres":genres}
+    return render(request,'book_library/genres.html',context )
