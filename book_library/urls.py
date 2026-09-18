d1b93bb271e6dea6241f2f7bed1c78b52b7cccc5
Christian <kalonjic34@gmail.com>
2026-09-18 15:10:12 +0200

    add genres URL path to urlpatterns for genre display

--- book_library/urls.py
+++ book_library/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "book_library"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("genres/",views.genres,name="genres")
 ]
