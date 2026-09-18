e34401913bc1b0d4e4c677d9f3fe014304ae9435
Christian <kalonjic34@gmail.com>
2026-09-18 14:28:46 +0200

    implement index view to display all books in the library

--- book_library/views.py
+++ book_library/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from .models import Book
 
-# Create your views here.
+def index(request):
+    books = Book.objects.all()
+    context = {"books":books}
+    return render(request,'book_library/index.html',context)
