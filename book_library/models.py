ded59acf275b806f3be0df270eca71b135be5ea1
Christian <kalonjic34@gmail.com>
2026-09-18 12:55:26 +0200

    add book model

--- book_library/models.py
+++ book_library/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_year = models.IntegerField()
+    description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+    def __str__(self)->str:
+        return self.title
