110215dbc9f29ea2d3eb1860e42a7c75119a0613
Christian <kalonjic34@gmail.com>
2026-09-18 13:28:15 +0200

    add genre model and link to book

--- book_library/models.py
+++ book_library/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
+    genre= models.ForeignKey(Genre,on_delete=models.CASCADE,null=True)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     published_year = models.IntegerField()
