c440da739818c3f6da89a0d8603386251dc533bb
Christian <kalonjic34@gmail.com>
2026-09-19 14:37:46 +0200

    add new views for genre and book creation with corresponding forms

--- book_library/views.py
+++ book_library/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.shortcuts import HttpResponse
+from book_library.forms import BookForm, GenreForm
 from .models import Book,Genre
 
 def index(request):
@@ -17,3 +18,32 @@ def genre(request,genre_id):
     books=genre.book_set.order_by('-date_added')
     context = {"genre":genre,'books':books}
     return render(request, "book_library/genre.html",context)
+def new_genre(request):
+    if request.method != 'POST':
+        form=GenreForm()
+    else:
+        form=GenreForm(data=request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('book_library:genres')
+        
+    context ={'form':form}
+    return render(request, 'book_library/new_genre.html', context)
+
+def new_book(request,genre_id):
+    genre = Genre.objects.get(id=genre_id)
+    if request.method != 'POST':
+        form=BookForm()
+    else:
+        form= BookForm(data=request.POST)
+        if form.is_valid():
+            new_book = form.save(commit=False)
+            new_book.genre=genre
+            new_book.save()
+            return redirect("book_library:genre", genre_id=genre.id)
+    context={
+        "genre":genre,
+        "form":form
+    }
+    return render(request, "book_library/new_book.html",context)
