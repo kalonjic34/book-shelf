f01887539108b20cafa8eef834cc2b10984904ee
Christian <kalonjic34@gmail.com>
2026-09-19 13:08:46 +0200

    add genre modelform

---
+++ book_library/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Genre
+
+class GenreForm(forms.ModelForm):
+    class Meta:
+        model= Genre
+        fields=["name"]
+        labels = {"name":""}
