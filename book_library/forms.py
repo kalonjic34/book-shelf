0d6cad530aa04bc3f26de027aee270b3c25404c9
Christian <kalonjic34@gmail.com>
2026-09-19 14:37:05 +0200

    add BookForm for book creation with detailed fields and widgets

--- book_library/forms.py
+++ book_library/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from .models import Genre
+from .models import Book, Genre
 
 class GenreForm(forms.ModelForm):
     class Meta:
         model= Genre
         fields=["name"]
         labels = {"name":""}
+        
+class BookForm(forms.ModelForm):
+    class Meta:
+        model=Book
+        fields=['title','author','published_year','description']
+        labels={
+            "title":"Title",
+            "author":"Author",
+            "published_year":"Year Published",
+            "description":"Description"
+        }
+        widgets={"description":forms.Textarea(attrs={"cols":80})}
